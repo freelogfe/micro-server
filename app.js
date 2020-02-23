abe93242bc057a40ed3c272b60245bfec316a161
wwzh <790727372@qq.com>
2020-02-24 03:16:38 +0800

    delete initialTrackRepositories

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 'use strict'
 
 const koaValidate = require('koa-validate')
-const initialTrackRepositories = require('./nodegit-core/initialTrackRepositories')
+// const initialTrackRepositories = require('./nodegit-core/initialTrackRepositories')
 
 module.exports = async app => {
   koaValidate(app)
   app.config.coreMiddleware.unshift('httpProxy')
   app.ready(async err => {
     if (err) throw err
-    await initialTrackRepositories(app.config.nodegit)
+    // await initialTrackRepositories(app.config.nodegit)
   })
 }
