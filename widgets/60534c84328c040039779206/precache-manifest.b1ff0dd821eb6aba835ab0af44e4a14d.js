0d52241c9c4d276484746211ea34759ebae29994
snnaenu <455342265@qq.com>
2021-03-30 17:40:10 +0800

    2

---
+++ widgets/60534c84328c040039779206/precache-manifest.b1ff0dd821eb6aba835ab0af44e4a14d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5041a961905d9bb0a046b9fd70f39e8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99409b975ecf7563390d28a751e07962",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "82e740fe0a3489d79895",
+    "url": "/static/css/app.0d24e2d9.css"
+  },
+  {
+    "revision": "a8d8b4b2ff764a19912f",
+    "url": "/static/css/chunk-vendors.ad33b5c4.css"
+  },
+  {
+    "revision": "82e740fe0a3489d79895",
+    "url": "/static/js/app.c1de5b85.js"
+  },
+  {
+    "revision": "a8d8b4b2ff764a19912f",
+    "url": "/static/js/chunk-vendors.775983a8.js"
+  }
+]);
